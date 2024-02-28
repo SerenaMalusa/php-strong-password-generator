28a7eb00f8597568e66b22b521ca2b3ed99800ed
SerenaMalusa <sere.malu@gmail.com>
2024-02-28 17:21:23 +0100

    feat: redirect to a new page

---
+++ new-password.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+
+     <!-- characters and viewport tags -->
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- SEO tags -->
+    <meta name="author" content="Serena Malusà" />
+    <meta name="description" content="php exercise">
+
+    <!-- title and icon -->
+    <title>PHP Generated Strong Password</title>
+    <link rel="icon" href="./imgs/logo-small.ico">
+
+    <!-- CSS -->
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+<body>
+
+    <h1>Ciao ciao</h1>
+    
+</body>
+</html>
--- partials/init.php
+++ partials/init.php
@@ -8,8 +8,13 @@ $user_password_length = $_GET['password-length'] ?? '';
 // if the form has been sent 
 if (!empty($_GET)) {
 
+    // redirect the user to the password page
+    header ('Location: /php-strong-password-generator/new-password.php');
+
     // create the password of the length chosen by the user
     $user_password = create_password($user_password_length);
+
+    exit;
 
 }  
 
